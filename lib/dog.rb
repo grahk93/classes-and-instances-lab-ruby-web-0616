46de803be815d34552caf769d4035b1e0e54a925
grahk93 <grace.juster@flatironschool.com>
2016-06-08 21:53:15 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog 
+  #This is a class that makes Dog objects and Dog methods 
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+class Person
+  #This is a class that makes Person objects and Person methods 
+end 
+
+adele_goldberg = Person.new
+alan_kay = Person.new
